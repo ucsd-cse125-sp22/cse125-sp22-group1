e2d094be8e998df0b18dd40b3c70866b28a0dcb0
Aaron James <5607614+KNOXDEV@users.noreply.github.com>
2022-04-05 10:26:45 -0700

    networking bootstrap (#3)
    * note about core's role in networking
    
    * added configuration file storage and access
    
    * game settings added for multiplayer information
    
    * primary server game loop scaffold
    
    * server -> client ping packet Proof of Concept

--- chariot-server/src/main.rs
+++ chariot-server/src/main.rs
@@ -1,3 +1,9 @@
+use chariot_core::GLOBAL_CONFIG;
+
+mod game;
+
 fn main() {
-    println!("Hello, world!");
+    // kick off the game loop
+    let ip_addr = format!("127.0.0.1:{}", GLOBAL_CONFIG.port);
+    game::GameServer::new(ip_addr).start_loop();
 }
