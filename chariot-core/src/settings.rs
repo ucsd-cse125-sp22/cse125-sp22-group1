b113327699b6fde1dae21365facb1138ce62d667
KNOXDEV <nick@knox.codes>
2022-04-04 19:07:05 -0700

    game settings added for multiplayer information

--- chariot-core/src/settings.rs
+++ chariot-core/src/settings.rs
@@ -6,14 +6,18 @@ use serde::Deserialize;
 pub struct Settings {
     pub port: String,
     pub server_address: String,
+    pub server_tick_ms: u64,
+    pub player_amount: usize
 }
 
 impl Settings {
     fn new() -> Result<Settings, ConfigError> {
         let config = Config::builder()
-            .set_default("port", "63686")?
+            .set_default("port", "24247")?
             .set_default("server_address", "127.0.0.1")?
-            .add_source(File::with_name("config.yaml"))
+            .set_default("server_tick_ms", 30)?
+            .set_default("player_amount", 1)?
+            .add_source(File::with_name("config.yaml").required(false))
             .build()?;
 
         config.try_deserialize()
