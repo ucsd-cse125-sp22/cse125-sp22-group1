38cda511fc8e5c4b4fa5dce3cfcf58dd5b65887a
Alistair Gray <alistairgray42@gmail.com>
2022-04-05 23:44:42 -0700

    Use global config settings framework instead of a random file with constants

--- chariot-core/src/settings.rs
+++ chariot-core/src/settings.rs
@@ -4,19 +4,40 @@ use serde::Deserialize;
 
 #[derive(Deserialize)]
 pub struct Settings {
+	// Networking
     pub port: String,
     pub server_address: String,
     pub server_tick_ms: u64,
-    pub player_amount: usize
+    pub player_amount: usize,
+
+	// Physics
+	pub gravity_coefficient: f64,
+	pub drag_coefficient: f64,
+	pub rolling_resistance_coefficient: f64,
+
+	pub rotation_reduction_coefficient: f64,
+
+	pub car_accelerator: f64,
+	pub car_brake: f64,
+	pub car_spin: f64,
 }
 
 impl Settings {
     fn new() -> Result<Settings, ConfigError> {
         let config = Config::builder()
+			// networking
             .set_default("port", "24247")?
             .set_default("server_address", "127.0.0.1")?
             .set_default("server_tick_ms", 30)?
             .set_default("player_amount", 1)?
+			// physics
+			.set_default("gravity_coefficient", 1.0)?
+			.set_default("drag_coefficient", 0.01)?
+			.set_default("rolling_resistance_coefficient", 0.3)?
+			.set_default("rotation_reduction_coefficient", 0.95)?
+			.set_default("car_accelerator", 1.0)?
+			.set_default("car_brake", 0.1)?
+			.set_default("car_spin", 0.1)?
             .add_source(File::with_name("config.yaml").required(false))
             .build()?;
 
--- chariot-server/src/physics/mod.rs
+++ chariot-server/src/physics/mod.rs
@@ -2,11 +2,10 @@ extern crate glam;
 
 use glam::DVec3;
 
+use chariot_core::GLOBAL_CONFIG;
 use chariot_core::physics_object::PhysicsProperties;
 use chariot_core::physics_object::EngineStatus;
 use chariot_core::physics_object::RotationStatus;
-
-mod constants;
 
 /* Given a set of physical properties, compute and return what next tick's
  * physics properties will be for that object */
@@ -17,9 +16,9 @@ pub fn do_physics_step(previous_props: &PhysicsProperties, time_step: f64) -> Ph
 	let velocity = previous_props.linear_momentum / previous_props.mass;
 
 	let angular_velocity: f64 = match previous_props.rotation_status {
-		RotationStatus::InSpinClockwise => previous_props.angular_velocity + constants::CAR_SPIN,
-		RotationStatus::InSpinCounterclockwise => previous_props.angular_velocity - constants::CAR_SPIN,
-		RotationStatus::NotInSpin => previous_props.angular_velocity * constants::ROTATION_REDUCTION_COEFFICIENT,
+		RotationStatus::InSpinClockwise => previous_props.angular_velocity + GLOBAL_CONFIG.car_spin,
+		RotationStatus::InSpinCounterclockwise => previous_props.angular_velocity - GLOBAL_CONFIG.car_spin,
+		RotationStatus::NotInSpin => previous_props.angular_velocity * GLOBAL_CONFIG.rotation_reduction_coefficient,
 	};
 
 	return PhysicsProperties {
@@ -47,7 +46,7 @@ pub fn sum_of_forces_on_object(object: &PhysicsProperties) -> DVec3
 
 
 fn gravitational_force_on_object(object: &PhysicsProperties) -> DVec3 {
-	return DVec3::new(0.0, -1.0, 0.0) * object.mass * constants::GRAVITY_COEFFICIENT;
+	return DVec3::new(0.0, -1.0, 0.0) * object.mass * GLOBAL_CONFIG.gravity_coefficient;
 }
 
 // unjustified temporary assumption we'll invalidate later: we're always on flat
@@ -60,7 +59,7 @@ fn normal_force_on_object(object: &PhysicsProperties) -> DVec3 {
 // force forwards
 fn tractive_force_on_object(object: &PhysicsProperties) -> DVec3 {
 	match &object.engine_status {
-		EngineStatus::Accelerating => return object.unit_steer_direction * object.mass * constants::CAR_ACCELERATOR,
+		EngineStatus::Accelerating => return object.unit_steer_direction * object.mass * GLOBAL_CONFIG.car_accelerator,
 		EngineStatus::Neutral => return DVec3::new(0.0, 0.0, 0.0),
 		EngineStatus::Braking => return DVec3::new(0.0, 0.0, 0.0),
 	}
@@ -69,12 +68,12 @@ fn tractive_force_on_object(object: &PhysicsProperties) -> DVec3 {
 fn air_resistance_force_on_object(object: &PhysicsProperties) -> DVec3
 {
 	// air resistance is proportional to the square of velocity
-	return object.velocity * object.mass * -1.0 * constants::DRAG_COEFFICIENT * object.velocity.length();
+	return object.velocity * object.mass * -1.0 * GLOBAL_CONFIG.drag_coefficient * object.velocity.length();
 }
 
 fn rolling_resistance_force_on_object(object: &PhysicsProperties) -> DVec3
 {
-	return object.velocity * object.mass * -1.0 * constants::ROLLING_RESISTANCE_COEFFICIENT;
+	return object.velocity * object.mass * -1.0 * GLOBAL_CONFIG.rolling_resistance_coefficient;
 }
 
 fn braking_resistance_force_on_object(object: &PhysicsProperties) -> DVec3 {
@@ -83,7 +82,7 @@ fn braking_resistance_force_on_object(object: &PhysicsProperties) -> DVec3 {
 		EngineStatus::Neutral => return DVec3::new(0.0, 0.0, 0.0),
 		// divide velocity by its magnitude to have a unit vector pointing
 		// opposite current heading
-		EngineStatus::Braking => return object.velocity / object.velocity.length() * -1.0 * object.mass * constants::CAR_BRAKE,
+		EngineStatus::Braking => return object.velocity / object.velocity.length() * -1.0 * object.mass * GLOBAL_CONFIG.car_brake,
 	}
 }
 
@@ -113,9 +112,9 @@ fn test_accelerating() {
 	// direction, and decreased because of drag and rolling resistance
 	let expected_velocity =
 		DVec3::new( 2.0,  0.0,  1.0) +
-		DVec3::new( 0.6,  0.0,  0.8) * constants::CAR_ACCELERATOR +
-		DVec3::new( -2.0,  0.0,  -1.0) * constants::DRAG_COEFFICIENT * (5.0 as f64).sqrt()  +
-		DVec3::new( -2.0,  0.0,  -1.0) * constants::ROLLING_RESISTANCE_COEFFICIENT;
+		DVec3::new( 0.6,  0.0,  0.8) * GLOBAL_CONFIG.car_accelerator +
+		DVec3::new( -2.0,  0.0,  -1.0) * GLOBAL_CONFIG.drag_coefficient * (5.0 as f64).sqrt()  +
+		DVec3::new( -2.0,  0.0,  -1.0) * GLOBAL_CONFIG.rolling_resistance_coefficient;
 	assert!(props.velocity.abs_diff_eq(expected_velocity, 0.001));
 	// momentum is just mass times velocity
 	assert!(props.linear_momentum.abs_diff_eq(expected_velocity * props.mass, 0.001));
@@ -146,8 +145,8 @@ fn test_non_accelerating() {
 	// - velocity should only have decreased, due to drag and rolling resistance
 	let expected_velocity =
 		DVec3::new( 2.0,  0.0,  1.0) +
-		DVec3::new( -2.0,  0.0,  -1.0) * constants::DRAG_COEFFICIENT * (5.0 as f64).sqrt()  +
-		DVec3::new( -2.0,  0.0,  -1.0) * constants::ROLLING_RESISTANCE_COEFFICIENT;
+		DVec3::new( -2.0,  0.0,  -1.0) * GLOBAL_CONFIG.drag_coefficient * (5.0 as f64).sqrt()  +
+		DVec3::new( -2.0,  0.0,  -1.0) * GLOBAL_CONFIG.rolling_resistance_coefficient;
 	assert!(props.velocity.abs_diff_eq(expected_velocity, 0.001));
 	// momentum is just mass times velocity
 	assert!(props.linear_momentum.abs_diff_eq(expected_velocity * props.mass, 0.001));
@@ -179,9 +178,9 @@ fn test_decelerating() {
 	let neg_prev_velocity = DVec3::new( -2.0,  0.0,  -1.0);
 	let expected_velocity =
 		prev_velocity +
-		(neg_prev_velocity / neg_prev_velocity.length()) * constants::CAR_BRAKE +
-		neg_prev_velocity * constants::DRAG_COEFFICIENT * (5.0 as f64).sqrt()  +
-		neg_prev_velocity * constants::ROLLING_RESISTANCE_COEFFICIENT;
+		(neg_prev_velocity / neg_prev_velocity.length()) * GLOBAL_CONFIG.car_brake +
+		neg_prev_velocity * GLOBAL_CONFIG.drag_coefficient * (5.0 as f64).sqrt()  +
+		neg_prev_velocity * GLOBAL_CONFIG.rolling_resistance_coefficient;
 	assert!(props.velocity.abs_diff_eq(expected_velocity, 0.001));
 	// momentum is just mass times velocity
 	assert!(props.linear_momentum.abs_diff_eq(expected_velocity * props.mass, 0.001));
@@ -204,9 +203,9 @@ fn test_spinning() {
 	};
 
 	props = do_physics_step(&props, 1.0);
-	assert_eq!(props.angular_velocity, constants::CAR_SPIN);
+	assert_eq!(props.angular_velocity, GLOBAL_CONFIG.car_spin);
 
 	props.rotation_status = RotationStatus::NotInSpin;
 	props = do_physics_step(&props, 1.0);
-	assert_eq!(props.angular_velocity, constants::CAR_SPIN * constants::ROTATION_REDUCTION_COEFFICIENT);
+	assert_eq!(props.angular_velocity, GLOBAL_CONFIG.car_spin * GLOBAL_CONFIG.rotation_reduction_coefficient);
 }
