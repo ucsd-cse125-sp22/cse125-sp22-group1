e2064621ac7f0eadcf9dc3f3824b1615d8628d3b
Alistair Gray <alistairgray42@gmail.com>
2022-04-09 23:48:51 -0700

    High-level overview of what the physics engine will be doing

---
+++ chariot-server/src/physics/physics_loop.rs
@@ -0,0 +1,5 @@
+// steps in loop:
+// 0. block for input and clock telling us to do a tick
+// 1. run one physics step on all players
+// 2. check if any pair of players are in collision
+// 3. if so, apply collision forces to them
