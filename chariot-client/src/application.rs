8f856612651636584537b3c3f7c52b49864103b2
nick <nnebel@ucsd.edu>
2022-04-23 14:25:08 -0700

    make it actually work

--- chariot-client/src/application.rs
+++ chariot-client/src/application.rs
@@ -9,10 +9,9 @@ use std::{
 use crate::drawable::*;
 use crate::renderer::*;
 use crate::resources::*;
-use crate::scenegraph::*;
 
 pub struct Application {
-    pub world: World,
+    pub drawables: Vec<StaticMeshDrawable>,
     pub renderer: Renderer,
     pub resources: ResourceManager,
 }
@@ -27,20 +26,8 @@ impl Application {
             panic!("Failed to import model");
         }
 
-        let mut world = World::new();
-        let mut helmet = Entity::new();
-        helmet.set_component(Transform {
-            translation: glam::Vec3::ZERO,
-            rotation: glam::Quat::from_axis_angle(glam::Vec3::X, f32::to_radians(90.0)),
-            scale: glam::vec3(0.3, 0.3, 0.3),
-        });
-
-        helmet.set_component(import_result.unwrap().drawables);
-
-        world.root_mut().add_child(helmet);
-
         Self {
-            world: world,
+            drawables: import_result.unwrap().drawables,
             renderer: renderer,
             resources: resources,
         }
@@ -51,48 +38,20 @@ impl Application {
             glam::Mat4::look_at_rh(glam::vec3(0.0, 0.0, -2.0), glam::Vec3::ZERO, glam::Vec3::Y);
         let proj = glam::Mat4::perspective_rh(f32::to_radians(60.0), 1.0, 0.1, 100.0);
         let proj_view = proj * view;
+        let model = glam::Mat4::IDENTITY;
 
         let mut render_job = render_job::RenderJob::new();
-        let root_transform = self
-            .world
-            .root()
-            .get_component::<Transform>()
-            .unwrap_or(&Transform::default())
-            .to_mat4();
-        dfs_acc(self.world.root_mut(), root_transform, |e, acc| {
-            let cur_model = e
-                .get_component::<Transform>()
-                .unwrap_or(&Transform::default())
-                .to_mat4();
-            let acc_model = *acc * cur_model;
-
-            if let Some(drawables) = e.get_component::<Vec<StaticMeshDrawable>>() {
-                for drawable in drawables.iter() {
-                    drawable.update_xforms(&self.renderer, &proj_view, &acc_model);
-                    let render_graph = drawable.render_graph(&self.resources);
-                    render_job.merge_graph(render_graph);
-                }
-            }
-
-            acc_model
-        });
+        for drawable in self.drawables.iter() {
+            drawable.update_xforms(&self.renderer, &proj_view, &model);
+            let render_graph = drawable.render_graph(&self.resources);
+            render_job.merge_graph(render_graph);
+        }
 
         self.renderer.render(&render_job);
     }
 
     pub fn update(&mut self) {
         //self.world.root_mut().update();
-        dfs_mut(self.world.root_mut(), &|e| {
-            if let Some(transform) = e.get_component::<Transform>() {
-                let rot_inc = glam::Quat::from_axis_angle(glam::Vec3::Y, 0.01);
-                let new_rot = rot_inc * transform.rotation;
-                let new_transform = Transform {
-                    rotation: new_rot,
-                    ..*transform
-                };
-                e.set_component(new_transform);
-            }
-        });
     }
 
     // TODO: input handlers
