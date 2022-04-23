280e6fa4599a4264d1647cb18e173f385c94baf6
nick <nnebel@ucsd.edu>
2022-04-23 14:39:56 -0700

    removed update() from Component trait, added basic application integration

--- chariot-client/src/application.rs
+++ chariot-client/src/application.rs
@@ -10,36 +10,76 @@ use std::{
 use crate::drawable::*;
 use crate::renderer::*;
 use crate::resources::*;
+use crate::scenegraph::*;
 
 pub struct Application {
-    pub drawables: Vec<StaticMeshDrawable>,
+    pub world: World,
     pub renderer: Renderer,
     pub resources: ResourceManager,
 }
 
 impl Application {
     pub fn new(renderer: Renderer) -> Self {
+        let mut resources = ResourceManager::new();
+
+        let import_result = resources.import_gltf(&renderer, "models/DamagedHelmet.glb");
+
+        if !import_result.is_ok() {
+            panic!("Failed to import model");
+        }
+
+        let mut world = World::new();
+        let mut helmet = Entity::new();
+        helmet.set_component(Transform {
+            translation: glam::Vec3::ZERO,
+            rotation: glam::Quat::from_axis_angle(glam::Vec3::X, f32::to_radians(90.0)),
+            scale: glam::vec3(0.3, 0.3, 0.3),
+        });
+
+        // temporarily commenting this since the new import stuff is in a different branch
+        //helmet.set_component(import_result.unwrap().drawables);
+
+        world.root_mut().add_child(helmet);
+
         Self {
-            drawables: Vec::new(),
+            world: world,
             renderer: renderer,
-            resources: ResourceManager::new(),
+            resources: resources,
         }
     }
 
     pub fn render(&mut self) {
         let view =
-            glam::Mat4::look_at_rh(glam::vec3(0.0, 0.0, -1.0), glam::Vec3::ZERO, glam::Vec3::Y);
-        let proj = glam::Mat4::perspective_rh(f32::to_radians(60.0), 1.0, 0.01, 1000.0);
+            glam::Mat4::look_at_rh(glam::vec3(0.0, 0.0, -2.0), glam::Vec3::ZERO, glam::Vec3::Y);
+        let proj = glam::Mat4::perspective_rh(f32::to_radians(60.0), 1.0, 0.1, 100.0);
         let proj_view = proj * view;
-        let model = glam::Mat4::from_translation(glam::vec3(0.0, -0.5, 0.0));
 
         let mut render_job = render_job::RenderJob::new();
-        for drawable in self.drawables.iter() {
-            drawable.update_xforms(&self.renderer, &proj_view, &model);
-            let render_item = drawable.render_item(&self.resources);
-            render_job.add_item(render_item);
-        }
+        let root_transform = self
+            .world
+            .root()
+            .get_component::<Transform>()
+            .unwrap_or(&Transform::default())
+            .to_mat4();
+        dfs_acc(self.world.root_mut(), root_transform, |e, acc| {
+            let cur_model = e
+                .get_component::<Transform>()
+                .unwrap_or(&Transform::default())
+                .to_mat4();
+            let acc_model = *acc * cur_model;
 
+            if let Some(drawables) = e.get_component::<Vec<StaticMeshDrawable>>() {
+                for drawable in drawables.iter() {
+                    drawable.update_xforms(&self.renderer, &proj_view, &acc_model);
+                    let render_item = drawable.render_item(&self.resources);
+                    render_job.add_item(render_item);
+                }
+            }
+
+            acc_model
+        });
+
+        self.renderer.render(&render_job);
         self.renderer.render(&render_job);
     }
 
--- chariot-client/src/scenegraph/mod.rs
+++ chariot-client/src/scenegraph/mod.rs
@@ -1,160 +1,202 @@
-use glam::Vec3;
+use crate::drawable::*;
 use std::any::{Any, TypeId};
 use std::boxed::Box;
 use std::collections::HashMap;
-use crate::drawable::*;
 
-struct World {
-	entity_count: usize,
-	entities: Vec<Box<Entity>>
+pub struct World {
+    root: Entity,
 }
 
-struct Entity {
-	components: HashMap<TypeId, Box<dyn Component>>,
-	children: Vec<Box<Entity>>
+pub struct Entity {
+    components: HashMap<TypeId, Box<dyn Component>>,
+    children: Vec<Entity>,
+}
 
-trait Component {
-    fn update(&mut self);
+pub trait Component {
     fn as_any(&self) -> &dyn Any;
 }
 
-
-struct Transform {
-	position: Vec3,
-	rotation: Vec3,
-	scale: Vec3,
+pub struct Transform {
+    pub translation: glam::Vec3,
+    pub rotation: glam::Quat,
+    pub scale: glam::Vec3,
 }
 
 impl Transform {
-	fn new() -> Self{
-		Self{
-			position: Vec3::ZERO,
-			rotation: Vec3::ZERO,
-			scale: Vec3::ZERO,
-		}
-	}
+    pub fn to_mat4(&self) -> glam::Mat4 {
+        glam::Mat4::from_scale_rotation_translation(self.scale, self.rotation, self.translation)
+    }
+}
+
+impl Default for Transform {
+    fn default() -> Self {
+        Self {
+            translation: glam::Vec3::ZERO,
+            rotation: glam::Quat::IDENTITY,
+            scale: glam::Vec3::ONE,
+        }
+    }
 }
 
 impl Component for Transform {
-    fn update(&mut self) {
-        //receive server data and update fields
-    }
-
     fn as_any(&self) -> &dyn Any {
         self
     }
 }
 
-
-struct PlayerController {
-	
-}
+struct PlayerController {}
 
 impl PlayerController {
-	fn new() -> Self{
-		Self{}
-	}
+    fn new() -> Self {
+        Self {}
+    }
 }
 
 impl Component for PlayerController {
-    fn update(&mut self) {
-        //send input data to server
-    }
-
     fn as_any(&self) -> &dyn Any {
         self
     }
 }
-
 
 impl Component for StaticMeshDrawable {
-    fn update(&mut self) {
-        //render entity
-    }
-
     fn as_any(&self) -> &dyn Any {
         self
     }
 }
 
-
-impl Entity {
-	pub fn new() -> Self {
-		Self{
-			components: HashMap::new(),
-			children: Vec::new(),
-		}
-	}
-
-	pub fn update(&mut self){
-		let map = &mut self.components;
-		for (key, mut component) in map{
-			component.update();
-		}
-	}
-
-	pub fn add_component<T: Component>(&mut self, component: T)
-		where
-			T: Component + 'static,
-	{
-		self.components.insert(TypeId::of::<T>(), Box::new(component));
-	}
-	
-	pub fn get_component<T: Component>(&self) -> Option<&T>
-		where
-			T: Component + 'static,
-	{
-		let id = TypeId::of::<T>();
-		self.components
-			.get(&id)
-			.map(|c| c.as_any().downcast_ref::<T>().unwrap())
-		
-	}
-
-	pub fn add_child(&mut self, entity: Entity) -> usize
-	{
-		let id = self.children.len();
-		self.children.push(Box::new(entity));
-		id
-	}
-	
-	pub fn get_child(&self, id: usize) -> &Entity
-	{
-		return self.children.get(id).unwrap();
-	}
-
+impl Component for Vec<StaticMeshDrawable> {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
 }
 
+pub struct Camera {
+    orbit_angle: glam::Vec2,
+    distance: f32,
+}
+
+impl Camera {
+    pub fn view_mat4(&self) -> glam::Mat4 {
+        let look_rot = glam::Quat::from_euler(
+            glam::EulerRot::XYZ,
+            self.orbit_angle.x,
+            self.orbit_angle.y,
+            0.0,
+        );
+        let look_dir = look_rot * glam::Vec3::Z;
+
+        let look_offset = look_dir * self.distance;
+
+        glam::Mat4::look_at_rh(look_offset, glam::Vec3::ZERO, glam::Vec3::Y)
+    }
+}
+
+impl Component for Camera {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl Entity {
+    pub fn new() -> Self {
+        Self {
+            components: HashMap::new(),
+            children: Vec::new(),
+        }
+    }
+
+    pub fn set_component<T: Component>(&mut self, component: T)
+    where
+        T: Component + 'static,
+    {
+        self.components
+            .insert(TypeId::of::<T>(), Box::new(component));
+    }
+
+    pub fn get_component<T: Component>(&self) -> Option<&T>
+    where
+        T: Component + 'static,
+    {
+        let id = TypeId::of::<T>();
+        self.components
+            .get(&id)
+            .map(|c| c.as_any().downcast_ref::<T>().unwrap())
+    }
+
+    pub fn add_child(&mut self, entity: Entity) -> usize {
+        let id = self.children.len();
+        self.children.push(entity);
+        id
+    }
+
+    pub fn get_child(&self, id: usize) -> &Entity {
+        return self.children.get(id).unwrap();
+    }
+}
 
 impl World {
-	pub fn new() -> Self {
-		Self{
-			entity_count: 0,
-			entities: Vec::new(),
-		}
-	}
+    pub fn new() -> Self {
+        Self {
+            root: Entity::new(),
+        }
+    }
 
-	pub fn add_entity(&mut self, entity: Entity) -> usize
-	{
-		let id = self.entity_count;
-		self.entities.push(Box::new(entity));
-		self.entity_count += 1;
-		id
-	}
-	
-	pub fn get_entity(&self, id: usize) -> &Entity
-	{
-		return self.entities.get(id).unwrap();
-	}
+    pub fn root(&self) -> &Entity {
+        &self.root
+    }
 
+    pub fn root_mut(&mut self) -> &mut Entity {
+        &mut self.root
+    }
+}
+
+pub fn dfs<'a, F>(root: &'a Entity, mut func: F)
+where
+    F: FnMut(&'a Entity),
+{
+    let mut entity_stack = vec![root];
+    while !entity_stack.is_empty() {
+        let cur_entity = entity_stack.pop().unwrap();
+        func(cur_entity);
+
+        entity_stack.extend(cur_entity.children.iter());
+    }
+}
+
+pub fn dfs_mut<'a, F>(root: &mut Entity, func: &F)
+where
+    F: Fn(&mut Entity),
+{
+    for child in root.children.iter_mut() {
+        func(child);
+        dfs_mut(child, func);
+    }
+}
+
+pub fn dfs_acc<'a, T, F>(root: &'a Entity, acc_init: T, mut func: F)
+where
+    F: FnMut(&'a Entity, &T) -> T,
+{
+    let mut entity_stack = vec![root];
+    let mut acc_stack = vec![acc_init];
+    while !entity_stack.is_empty() {
+        let cur_entity = entity_stack.pop().unwrap();
+        let cur_acc = acc_stack.pop().unwrap();
+
+        for child in cur_entity.children.iter() {
+            let acc = func(child, &cur_acc);
+            acc_stack.push(acc);
+            entity_stack.push(child);
+        }
+    }
 }
 
 #[test]
 fn test_entity_child() {
-	let mut world = World::new();
-	let mut entity = Entity::new();
-	let mut child = Entity::new();
-	let child_idx = entity.add_child(child);
-	let entity_idx = world.add_entity(entity);
-	world.get_entity(entity_idx).get_child(child_idx);
+    let mut world = World::new();
+    let mut entity = Entity::new();
+    let mut child = Entity::new();
+    let child_idx = entity.add_child(child);
+    //let entity_idx = world.add_entity(entity);
+    //world.get_entity(entity_idx).get_child(child_idx);
 }
