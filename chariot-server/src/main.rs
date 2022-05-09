dd7943eb4b467e48ada8f3e0e21b678a8f33b383
Aaron James <5607614+KNOXDEV@users.noreply.github.com>
2022-05-09 15:06:15 -0700

    dont look at the change log unless you want instant suffering (#33)

--- chariot-server/src/main.rs
+++ chariot-server/src/main.rs
@@ -6,6 +6,6 @@ mod physics;
 
 fn main() {
     // kick off the game loop
-    let ip_addr = format!("127.0.0.1:{}", GLOBAL_CONFIG.port);
+    let ip_addr = format!("0.0.0.0:{}", GLOBAL_CONFIG.port);
     game::GameServer::new(ip_addr).start_loop();
 }
