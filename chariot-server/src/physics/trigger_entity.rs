4df2735bbc9f03da1b55aa4ce2ce4c26be9899c5
crossboy <itscrossboy@gmail.com>
2022-05-09 17:28:11 -0700

    Address requested changes

--- chariot-server/src/physics/trigger_entity.rs
+++ chariot-server/src/physics/trigger_entity.rs
@@ -6,12 +6,12 @@ type BoundingBoxDimensions = [[f64; 2]; 3];
 pub trait TriggerEntity {
     fn get_bounding_box(&self) -> BoundingBoxDimensions;
 
-    fn check_bounding_box_collisions(&self, ply: &PlayerEntity) -> bool {
+    fn check_bounding_box_collisions(&self, colliding_player: &PlayerEntity) -> bool {
         let mut collision_dimensions = [false, false, false];
 
         for dimension in 0..=2 {
             let [min_1, max_1] = self.get_bounding_box()[dimension];
-            let [min_2, max_2] = ply.bounding_box[dimension];
+            let [min_2, max_2] = colliding_player.bounding_box[dimension];
 
             if {
                 (min_2 <= min_1 && min_1 <= max_2) // min_1 is inside 2
@@ -26,5 +26,5 @@ pub trait TriggerEntity {
         return collision_dimensions.iter().all(|&x| x);
     }
 
-    fn trigger(&self, ply: &mut PlayerEntity);
+    fn trigger(&self, colliding_player: &mut PlayerEntity);
 }
