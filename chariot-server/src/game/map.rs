38832e39f2faca91adc9da47efe1a92d5eac6eae
crossboy <itscrossboy@gmail.com>
2022-05-11 22:03:52 -0700

    Only process meshes on the server that have the property collision set to 1

--- chariot-server/src/game/map.rs
+++ chariot-server/src/game/map.rs
@@ -10,35 +10,33 @@ pub struct Map {
 
 impl Map {
     //pub fn load(filename: &str) -> Result<Map, gltf::Error> {
-    pub fn load(filename: &str) {
+    pub fn load(filename: String) {
         println!(
             "loading {}, please give a sec I swear it's not lagging",
             filename
         );
         let model_name = filename.split(".").next().expect("invalid filename format");
-        let (document, buffers, images) = gltf::import(filename).unwrap();
+        let (document, buffers, images) = gltf::import(filename).expect("Failed in import map");
 
         //let mut mesh_handles = Vec::new();
         for (mesh_idx, mesh) in document.meshes().enumerate() {
-            println!(
-                "processing mesh {}",
-                mesh.name().unwrap_or("[a mesh that's not named]")
-            );
-
-            if mesh.primitives().len() != 1 {
-                print!(
-                    "Warning: I'm expecting one prim per mesh so things might not work properly"
-                );
-            }
-
-            for (prim_idx, primitive) in mesh.primitives().enumerate() {
-                println!("\tprocessing prim {}", prim_idx);
-                //let handle = self.import_mesh(renderer, &buffers, &primitive);
-                //mesh_handles.push(handle);
-            }
-
             if let Some(extras) = mesh.extras().as_ref() {
                 let mesh_data: Value = serde_json::from_str(extras.as_ref().get()).unwrap();
+                if mesh_data["collision"] == 0 {
+                    continue;
+                }
+
+                println!(
+                    "processing mesh {}",
+                    mesh.name().unwrap_or("[a mesh that's not named]")
+                );
+
+                if mesh.primitives().len() != 1 {
+                    print!(
+						"Warning: I'm expecting one prim per mesh so things might not work properly"
+					);
+                }
+
                 println!("X: {:?}", mesh_data["render"] == 0);
             }
         }
