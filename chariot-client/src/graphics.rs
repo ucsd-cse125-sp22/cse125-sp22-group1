5bce0e6ea9be0f41a87cc3156463837d986136e4
Alistair J Gray <ajgray@UCSD.EDU>
2022-05-21 21:00:33 -0700

    Massage position values some more

--- chariot-client/src/graphics.rs
+++ chariot-client/src/graphics.rs
@@ -339,10 +339,10 @@ impl GraphicsManager {
         // Convert "map units" locations into proportions of minimap size
         fn get_minimap_player_location(location: (f32, f32)) -> (f32, f32) {
             // these values are guesses btw
-            const MIN_TRACK_X: f32 = -113.0;
-            const MAX_TRACK_X: f32 = 37.0;
-            const MIN_TRACK_Z: f32 = -39.0;
-            const MAX_TRACK_Z: f32 = 111.0;
+            const MIN_TRACK_X: f32 = -119.0; // top
+            const MAX_TRACK_X: f32 = 44.0; // bottom
+            const MIN_TRACK_Z: f32 = -48.0; // right
+            const MAX_TRACK_Z: f32 = 119.0; // left
 
             (
                 (MAX_TRACK_Z - location.1) / (MAX_TRACK_Z - MIN_TRACK_Z),
