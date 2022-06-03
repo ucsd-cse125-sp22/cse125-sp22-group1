58db82d63f80717a5cc90135ef10f77309215359
Wenlin Mao <w6mao@ucsd.edu>
2022-06-03 08:13:06 -0700

    display final placement times in the right order

--- chariot-client/src/ui_state.rs
+++ chariot-client/src/ui_state.rs
@@ -838,6 +838,8 @@ impl GraphicsManager {
             background_texture,
         )];
 
+        // positions and times both indexed by player_num
+
         let mut player_nums: Vec<usize> = vec![0, 1, 2, 3];
         player_nums.sort_by(|&a, &b| positions[a].cmp(&positions[b]));
 
@@ -936,18 +938,20 @@ impl GraphicsManager {
             let millis = time_millis % 1000;
             let time_str = format!("{:02}:{:02}:{:03}", minutes, seconds, millis);
 
+            let placement_index = positions[player_index];
+
             // don't worry, i hate this code too
-            if player_index == 0 {
+            if placement_index == 0 {
                 P1_FINAL_TIME_TEXT
                     .clone()
                     .content(&time_str)
                     .build_drawable(&self.renderer, &mut self.resources)
-            } else if player_index == 1 {
+            } else if placement_index == 1 {
                 P2_FINAL_TIME_TEXT
                     .clone()
                     .content(&time_str)
                     .build_drawable(&self.renderer, &mut self.resources)
-            } else if player_index == 2 {
+            } else if placement_index == 2 {
                 P3_FINAL_TIME_TEXT
                     .clone()
                     .content(&time_str)
