8bed44ed5c30a00bcacab47fb0e74c2b8b1c957d
Ronak Shah <rmshah@ucsd.edu>
2022-06-02 22:34:44 -0700

    everyone gets the effects (#101)

--- chariot-client/src/application.rs
+++ chariot-client/src/application.rs
@@ -72,11 +72,6 @@ impl Application {
             self.graphics.update_timer(since_game_started);
         }
 
-        // TODO: do this for other players
-        if self.pressed_keys.contains(&VirtualKeyCode::W) {
-            self.graphics.add_fire_to_player(0, delta_time);
-        }
-
         self.last_update = SystemTime::now();
 
         self.game.fetch_incoming_packets();
@@ -131,10 +126,16 @@ impl Application {
                 }
 
                 ClientBoundPacket::EntityUpdate(locations) => {
-                    locations.iter().enumerate().for_each(|(i, update)| {
-                        self.graphics
-                            .update_player_location(&update.0, &update.1, i)
-                    });
+                    locations
+                        .iter()
+                        .enumerate()
+                        .for_each(|(i, (location, velocity, did_move))| {
+                            self.graphics
+                                .update_player_location(&location, &velocity, i);
+                            if *did_move {
+                                self.graphics.add_fire_to_player(i, delta_time);
+                            }
+                        });
                 }
                 ClientBoundPacket::PlacementUpdate(given_position) => {
                     let position = if given_position > 4 {
