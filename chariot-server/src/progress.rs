d8a6551a5022f298693b2925eb10215d1941914f
Alistair J Gray <ajgray@UCSD.EDU>
2022-06-02 21:59:09 -0700

    Only use checkpoints, not zones

--- chariot-server/src/progress.rs
+++ chariot-server/src/progress.rs
@@ -6,16 +6,11 @@ use chariot_core::player::{lap_info::*, PlayerID};
 
 impl PlayerEntity {
     // Values returned aren't intended to be interpreted directly, only compared
-    fn get_progress_score(
-        &self,
-        checkpoints: &Vec<Checkpoint>,
-    ) -> (LapNumber, ZoneID, CheckpointID, f64) {
-        // There's a hierarchy of four pieces of progress information we have:
-        // from least to most granular lap number, zone number (zones are
-        // between major checkpoints), minor checkpoint number, and linear
-        // interpolation between minor checkpoint
+    fn get_progress_score(&self, checkpoints: &Vec<Checkpoint>) -> (LapNumber, CheckpointID, f64) {
+        // There's a hierarchy of three pieces of progress information we have:
+        // from least to most granular lap number, minor checkpoint number, and
+        // linear interpolation between minor checkpoint
         let lap_number = self.lap_info.lap;
-        let zone_number = self.lap_info.zone;
         let checkpoint_number = self.lap_info.last_checkpoint;
         let mut position_within_checkpoint = 0.0;
 
@@ -34,12 +29,7 @@ impl PlayerEntity {
             position_within_checkpoint = player_relative_location.project_onto(trackline).length();
         }
 
-        return (
-            lap_number,
-            zone_number,
-            checkpoint_number,
-            position_within_checkpoint,
-        );
+        (lap_number, checkpoint_number, position_within_checkpoint)
     }
 }
 
@@ -47,17 +37,16 @@ pub fn get_player_placement_array(
     players: &[PlayerEntity; 4],
     checkpoints: &Vec<Checkpoint>,
 ) -> [(usize, LapInformation); 4] {
-    let mut player_nums_with_scores: Vec<(PlayerID, (LapNumber, ZoneID, CheckpointID, f64))> =
-        [0, 1, 2, 3]
-            .into_iter()
-            .zip(players.iter().map(|p| p.get_progress_score(checkpoints)))
-            .collect();
+    let mut player_nums_with_scores: Vec<(PlayerID, (LapNumber, CheckpointID, f64))> = [0, 1, 2, 3]
+        .into_iter()
+        .zip(players.iter().map(|p| p.get_progress_score(checkpoints)))
+        .collect();
 
     // Sort progress scores, priority given to most significant placement measures
     player_nums_with_scores.sort_by(|a, b| {
-        let a_arr = [a.1 .0 as f64, a.1 .1 as f64, a.1 .2 as f64, a.1 .3];
+        let a_arr = [a.1 .0 as f64, a.1 .1 as f64, a.1 .2 as f64];
         let a_finished = players[a.0].lap_info.finished;
-        let b_arr = [b.1 .0 as f64, b.1 .1 as f64, b.1 .2 as f64, b.1 .3];
+        let b_arr = [b.1 .0 as f64, b.1 .1 as f64, b.1 .2 as f64];
         let b_finished = players[b.0].lap_info.finished;
 
         if a_finished || b_finished {
@@ -75,7 +64,7 @@ pub fn get_player_placement_array(
                 }
             }
         } else {
-            for index in 0..=3 {
+            for index in 0..=2 {
                 if a_arr[index] < b_arr[index] {
                     return std::cmp::Ordering::Less;
                 } else if a_arr[index] > b_arr[index] {
@@ -87,20 +76,12 @@ pub fn get_player_placement_array(
     });
 
     return [0, 1, 2, 3].map(|i| {
-        let data = player_nums_with_scores.get(i).unwrap();
-        if players[data.0].lap_info.finished {
-            (data.0, players[data.0].lap_info)
-        } else {
-            (
-                data.0,
-                LapInformation {
-                    lap: data.1 .0,
-                    zone: data.1 .1,
-                    last_checkpoint: data.1 .2,
-                    placement: 4 - i as Placement,
-                    finished: false,
-                },
-            )
-        }
+        let &(player_num, _) = player_nums_with_scores.get(i).unwrap();
+        let mut old_lap_info = players[player_num].lap_info;
+        if !old_lap_info.finished {
+            old_lap_info.placement = 4 - i as Placement;
+        };
+
+        (player_num, old_lap_info)
     });
 }
